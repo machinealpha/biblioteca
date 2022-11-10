47184bb23d885befa98b2f5dfc0bcef00c55257f
Marcelo Fernandez <marcelo.fernandez@global66.com>
2022-11-09 23:46:10 -0300

    mfernpoo: cambio constructor

--- src/main/java/Estudiante.java
+++ src/main/java/Estudiante.java
@@ -1,8 +1,8 @@
 public class Estudiante extends Usuario
 {private String carrera;
 
-    public Estudiante(String nombre, String rut, String genero, String carrera) {
-        super(nombre, rut, genero);
-        this.carrera = carrera;
+    public Estudiante(String nombre, String rut, String genero, String carrera, int prestamo, String carrera1) {
+        super(nombre, rut, genero, carrera, prestamo);
+        this.carrera = carrera1;
     }
 }
